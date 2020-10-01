295ef0d7c3c01dd8a1d2df51c3edc2310408dec1
Peter Rosendahl <p.rosendahl.89@gmail.com>
2020-10-01 21:04:59 +0000

    Converted to functional component.

--- src/Modules/Whiteboard/Tab/TabItem.js
+++ src/Modules/Whiteboard/Tab/TabItem.js
@@ -1,19 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TabItem.css';
 import CloseIcon from '@material-ui/icons/Close';
 
-class TabItem extends Component {
+export default function TabItem(props) {
 
-    
-    render() {
-        return (
-            <div className={`tab-item ${this.props.isActive ? "active" : ""}`}
-                onClick={() => this.props.onTabClick(this.props.id)}>
-                <span className='tab-name'>{this.props.name}</span>
-                <CloseIcon className="icon clickable" onClick={() => this.props.closeClicked(this.props.id)} />
-            </div>
-        )
-    }
-}
-
-export default TabItem;
+    return (
+        <div className={`tab-item ${props.isActive ? "active" : ""}`}
+            onClick={() => props.onTabClick(props.id)}>
+            <span className='tab-name'>{props.name}</span>
+            <CloseIcon className="icon clickable" onClick={() => props.closeClicked(props.id)} />
+        </div>
+    )
+};
